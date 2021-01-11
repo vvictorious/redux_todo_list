afb54ed8178439b8aed4e1d5226aff3d741f71ba
vvictorious <vvictorious30@gmail.com>
2021-01-11 15:08:42 -0700

    reducer should be good. moving onto the store and wrapping the app in the provider

---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+import {
+    ADD_TODO,
+    DELETE_TODO,
+    UPDATE_TODO
+} from './actions'
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            break
+        case DELETE_TODO:
+            break
+        case UPDATE_TODO:
+            break
+        default:
+            return state
+    }
+}
