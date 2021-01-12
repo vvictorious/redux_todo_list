cffcfba7c0909be346d57a874de57c125286aa95
vvictorious <vvictorious30@gmail.com>
2021-01-12 14:59:58 -0700

    sheesh, updatee is alwayss so many steps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteTodo } from './redux/actions'
 
 const TodoItem = ({todo}) => {
 
+    const [edit, setEdit] = useState(false)
+    const [name, setName] = useState(todo.name)
+
     const dispatch = useDispatch()
 
     return (
         <div className='todoItem__container'>
-            <h5>#{todo.id.length > 1 ? todo.id[2]: todo.id}</h5>            
-            <p>{todo.name}</p>
+            <h5>#{todo.id.length > 1 ? todo.id[2]: todo.id}</h5>
             <div>
-                <button>Edit</button>
+                {
+                    edit ? 
+                        <input type='text' 
+                        placeholder={todo.name} 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                        />                     
+                    : 
+                        <p>{todo.name}</p>    
+                }                            
+            </div>      
+            <div>
+                <button onClick={ () => {
+                    if (edit) {
+                        setName('')
+                    }
+                    setEdit(!edit)
+                }}>
+
+                    {
+                        edit ?
+                            'update'
+                        :
+                            'edit'
+                    }
+                </button>
                 <button onClick={ () => dispatch(deleteTodo(todo.id))}>Delete</button>
             </div>
         </div>
